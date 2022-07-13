de3bf999e49b837b2686369e3dd1aa12585451cb
Rizalrubyrahman <rizalrubyr@gmail.com>
2022-07-13 16:52:06 +0700

    membuat tabel transaksi penjualan

---
+++ app/SaleTransaction.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SaleTransaction extends Model
+{
+    protected $primaryKey = 'payment_method_id';
+}
---
+++ database/migrations/2022_07_13_093246_create_sale_transactions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSaleTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_transactions', function (Blueprint $table) {
+            $table->id('sale_transaction_id');
+            $table->foreignId('product_id')->nullable();
+            $table->foreignId('payment_method_id')->nullable();
+            $table->foreignId('sale_channel_id')->nullable();
+            $table->date('date');
+            $table->string('sale_amount');
+            $table->string('expense_amount')->nullable();
+            $table->string('note')->nullable();
+            $table->string('status')->nullable();
+            $table->string('file')->nullable();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale_transactions');
+    }
+}
